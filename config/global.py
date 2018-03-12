bad5735059cb1c28c69f3e795036a22e3292f669
Chang <584911924@qq.com>
2018-03-12 19:48:15 +0800

    测试python

---
+++ config/global.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+execlPath = './excel/企业信息.xlxs'
+
+userName = 'LCS2018'
+userPassword = '123456'
+
+userUrl = 'http://data.develop.wei3dian.com'
+
+assestPath = os.path.dirname(os.path.dirname(__file__))  # 文件根目录
+
+print(assestPath)
