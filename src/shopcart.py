6da3573d4eef44cbcb518481536b2ab08f18e7cc
Chang <584911924@qq.com>
2018-03-15 17:46:24 +0800

    实现了新开窗口的自动化，想办法在原有基础上自动化

--- src/shopcart.py
+++ src/shopcart.py
@@ -1,22 +1,20 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
 
+import time
 import os  # 文件、目录模块
 import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../..')))
-
-from config import Config
-
 import xlrd  # 读取 excel
 from selenium import webdriver  # python 启动浏览器
 from threading import Timer  # 定时器
 
-from pywinauto import application  # C/S 软件操作
+assestpath = os.path.abspath(os.path.join(__file__, '../..'))  # 工程根目录
+sys.path.insert(0, assestpath)
 
-driver = webdriver.Chrome()  # 打开谷歌
-# driver = webdriver.Ie()  # 打开IE
+from config import Config
+
+# driver = webdriver.Chrome()  # 打开谷歌
+driver = webdriver.Ie()  # 打开IE
 
 # Load page 在地址栏输入http://www.baidu.com 进入百度
 driver.get(Config.userurl)
@@ -39,79 +37,90 @@ driver.find_element_by_class_name('login_box_r').click()  # 关掉ca登录
 time.sleep(1)
 driver.find_element_by_class_name('btn-block').click()  # 找到对应的按钮  登陆
 time.sleep(1)
+pass
 
-driver.find_element_by_class_name('fa-cubes').click()  # 找到对应的按钮  数据中心 进入系统
-time.sleep(5)
+# 选择系统
+link_ul = driver.find_element_by_class_name('link')
+link_li = link_ul.find_elements_by_tag_name('li')
 
-menu_first = driver.find_element_by_name('企业机构库')
-menu_first.click()
-time.sleep(1)
-menu_first.find_element_by_name('生产企业').click()  # 找到 企业机构库》生产企业  点击
-time.sleep(1)
+for item in link_li:
+    if item.text == '东莞交易':
+        item.click()
+    pass
+time.sleep(5)  # 等待时间长点，免得找不到购物车可以点
+pass
+
+# 进入购物车
+driver.find_element_by_class_name('w_car_main').click()
+time.sleep(2)
+pass
+
 
 xlsx_path = os.path.abspath(os.path.join(
-    __file__, '../..', 'docs', 'excel', '企业信息.xls'))
+    assestpath, 'docs', 'excel', '订单明细目录.xls'))
 data = xlrd.open_workbook(xlsx_path)
 
 table = data.sheet_by_index(0)  # 通过索引获取excel的sheet
 
 nrows = table.nrows
 
-
-def set_form_value(data):
-    # 设置表单的value
-    driver.find_element_by_name('create').click()
-    time.sleep(1)
-    pass
-    fromtemp = driver.find_element_by_class_name('fromtemp')
-    formitem_content = fromtemp.find_elements_by_class_name(
-        'el-form-item__content')
-    for item in formitem_content:
-        try:
-            _input = item.find_element_by_css_selector('input')
-            _input.send_keys('你好啊')
-            pass
-        except Exception as error:  # 处理常规异常
-            print(str(error))
-            pass
-        except BaseException as error:  # 所有异常基类
-            print(str(error))
-            pass
-        pass
-    time.sleep(1)
-    pass
-
-
-table_key = []
-
-
-def get_table_json(row):
-    # 获取excel每一行相对应的json
-    i = 0
-    json = {}
-    while i < len(table_key):
-        json[table_key[i]] = row[i]
-        i += 1
-        pass
-    pass
-    return json
-
-
+table_index = 0
+drug_arr = []
 i = 0
 while i < nrows:
     if i > 0:
         row = table.row_values(i)  # 1, 生产企业， 生产， 广东省,深圳市,南山区
-        data = get_table_json(row)
-        set_form_value(data)
+        drug_arr.append(row[table_index])
         pass
     else:
-        table_key = table.row_values(i)
+        table_index = table.row_values(i).index('药品编码')
         pass
     i += 1
     pass
 
-print(data)
 
-time.sleep(5)
+def find_drugs(code):
+  # 查询药品编码
+    driver.find_element_by_class_name('fromtrades').find_element_by_name(
+        'isCreatShowTrade').click()  # 点击添加明细
+    time.sleep(1)
+    pass
+    driver_input = driver.find_element_by_class_name('dialog-list').find_element_by_css_selector(
+        'div[data-name=drugsCode]').find_element_by_tag_name('input')
 
-driver.quit()
+    driver_input.clear()
+    # 把code转list，一个一个send_key进去
+    code_list = list(code)
+    for value in code_list:
+        driver_input.send_keys(value)
+        pass
+
+    time.sleep(1)
+    pass
+    driver.find_element_by_class_name(
+        'dialog-list').find_element_by_name('dialogBtn').click()
+    time.sleep(1)
+    pass
+    driver.find_element_by_class_name('dialog-list').find_element_by_tag_name(
+        'thead').find_element_by_class_name('el-checkbox').click()
+    time.sleep(1)
+    pass
+    driver.find_element_by_class_name(
+        'dialog-list').find_element_by_name('isShowSure').click()
+    time.sleep(1)
+    pass
+    print(code)
+    pass
+
+
+i = 0
+while i < len(drug_arr):
+    find_drugs(drug_arr[i])
+    i += 1
+    pass
+
+# print(data)
+
+# time.sleep(5)
+
+# driver.quit()
